07f9c3c225049c9c3f1f42662a61dd147f971944
phongsiriNet <151302483+phongsiriNet@users.noreply.github.com>
2024-06-23 10:51:07 +0700

    Update layout.tsx
    Change title of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "John Farmer",
+  title: "Patitek",
   description: "Example personal website",
 };
 
