1a3c8d1a1c3258218b663302620d9c538d379edd
phongsiriNet <151302483+phongsiriNet@users.noreply.github.com>
2024-06-23 10:52:33 +0700

    Update layout.tsx
    change title name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Patitek",
+  title: "Patitek--",
   description: "Example personal website",
 };
 
